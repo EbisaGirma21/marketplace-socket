91593cd8ed521d7c55a582aea36a1ad623039e04
Ebisa Girma <ebisagirma41@gmail.com>
2024-05-15 22:39:23 +0300

    socket

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const socketIo = require("socket.io");
 const app = express();
 const server = http.createServer(app);
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Method", "POST,GET,PUT,DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+
 const io = socketIo(server, {
   cors: {
     origin: "*",
